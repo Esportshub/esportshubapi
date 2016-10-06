8fb4bf0aaea66516ae8b251e2bdc4af63b4b0b31
Matti andreas nielsen <mattinielsen5@hotmail.com>
2016-10-06 20:22:30 +0200

    Entities: Team, Game, Event

---
+++ Models/Entities/Event/Event.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Models.Entities {
+
+    public class Event {
+                public int id { get; set; }
+                public string Name { get; set; }
+                
+                public Guid Guid { get; set; }
+
+                public DateTime Start { get; set; }
+            
+                public DateTime End { get; set; }
+            
+                public DateTime Created { get; set; }
+            
+                public DateTime Updated { get; set; }
+    }
+}
