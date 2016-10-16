98d999f8e85c35d98afeb36f81f1d6893139c245
Sjuften <martinbergpetersen@gmail.com>
2016-10-16 16:48:46 +0200

    Adding all the controllers

--- RestfulApi/Controllers/TeamController.cs
+++ RestfulApi/Controllers/TeamController.cs
@@ -1,19 +1,16 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Models.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace esportshubapi.Controllers
 {
     [Route("api/[controller]")]
-    public class PlayerController : Controller
+    public class TeamController : Controller
     {
-        internal IPlayerRepository _playerRepository;
-        public PlayerController(IPlayerRepository playerRepository) 
-        {
-            _playerRepository = playerRepository;
-        }
-
-
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
