0e7d9c132f020bcd98f4de8dae0b21927bf59eb4
Sjuften <martinbergpetersen@gmail.com>
2016-10-19 15:22:33 +0200

    project.json

--- Test/RestfulApi/Entities/Game/Game.cs
+++ Test/RestfulApi/Entities/Game/Game.cs
@@ -1,11 +1,14 @@
 using Xunit;
+using RestFul;
 namespace Test.RestFul.Entities
 {
     public class Game
     {
         [Fact]
-        public void GameBuilder()
+        public void GamdfeBuilder()
         {
+            var s = Game.Builder().Build();
+            df
 
         }
     
