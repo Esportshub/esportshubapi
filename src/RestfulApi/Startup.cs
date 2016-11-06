0c2f1c1a0cc737030126975ad60a14e2002ced91
Matti andreas nielsen <mattinielsen5@hotmail.com>
2016-11-06 05:14:27 +0100

    Getting close to making the endpoint return some json i think.

--- src/RestfulApi/Startup.cs
+++ src/RestfulApi/Startup.cs
@@ -16,7 +16,6 @@ using Moq;
 using EsportshubApi.Models.Repositories;
 using EsportshubApi.Models.Entities;
 
-
 namespace esportshubapi
 {
     public class Startup
@@ -25,6 +24,7 @@ namespace esportshubapi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore();
             var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json").Build();
@@ -50,11 +50,6 @@ namespace esportshubapi
             app.UseMvc(routes =>
                  routes.MapRoute("player", "{controller=Player}/{action=Get}/{id?}")
             );     
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
     }
 }
