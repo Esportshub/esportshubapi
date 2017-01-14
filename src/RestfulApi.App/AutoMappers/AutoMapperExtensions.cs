7c1edae39935e11efa482a6096d17806c337641d
Sjuften <martinbergpetersen@gmail.com>
2017-01-14 19:52:46 +0100

    new structure

---
+++ src/RestfulApi.App/AutoMappers/AutoMapperExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace RestfulApi.App.AutoMapper
+{
+    public static class AutoMapperExtensions
+    {
+        public static T MapTo<T>(this T to)
+        {
+            
+        }
+    }
+}
