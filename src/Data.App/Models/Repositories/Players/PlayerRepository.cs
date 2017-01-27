b8695cedbd4407ea745d768c77eadb645dd287d0
Matti andreas nielsen <mattinielsen5@hotmail.com>
2017-01-27 17:13:54 +0100

    changing playerRepository to use composition instead of inheritance

--- src/Data.App/Models/Repositories/Players/PlayerRepository.cs
+++ src/Data.App/Models/Repositories/Players/PlayerRepository.cs
@@ -1,12 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 using Data.App.Models.Entities;
 
 namespace Data.App.Models.Repositories.Players
 {
-    public class PlayerRepository : GenericRepository<Player>, IPlayerRepository
+    public class PlayerRepository : IPlayerRepository
     {
-        public PlayerRepository(EsportshubContext context) : base(context)
+        private readonly IRepository<Player> _internalRepository;
+        private readonly EsportshubContext _esportshubContext;
+
+        public PlayerRepository(EsportshubContext context, IRepository<Player> internalRepository)
         {
-            
+            _esportshubContext = context;
+            _internalRepository = internalRepository;
+        }
+
+        public async Task<IEnumerable<Player>> FindByAsync(Expression<Func<Player, bool>> filter, string includeProperties)
+        {
+            return await _internalRepository.FindByAsync(filter, includeProperties);
+        }
+
+        public async Task<Player> FindAsync(int id)
+        {
+            return await _internalRepository.FindAsync(id);
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            return await _internalRepository.SaveAsync();
+        }
+
+        public IEnumerable<Player> FindBy(Expression<Func<Player, bool>> filter, string includeProperties)
+        {
+            return _internalRepository.FindBy(filter, includeProperties);
+        }
+
+        public Player Find(int id)
+        {
+            return _internalRepository.Find(id);
+        }
+
+        public void Insert(Player entity)
+        {
+            _internalRepository.Insert(entity);
+        }
+
+        public void Delete(int id)
+        {
+            _internalRepository.Delete(id);
+        }
+
+        public void Update(Player entity)
+        {
+            _internalRepository.Update(entity);
+        }
+
+        public bool Save()
+        {
+            return _internalRepository.Save();
         }
     }
 }
